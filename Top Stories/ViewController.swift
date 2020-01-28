83e19e56f944bdd97dc7837768a13ac84e0c789e
Ace <cferrari2bsd220.org>
2020-01-28 10:42:45 -0600

    Initial Commit

---
+++ Top Stories/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Top Stories
+//
+//  Created by Christina Ferrari on 1/28/20.
+//  Copyright © 2020 TheLastAce. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
